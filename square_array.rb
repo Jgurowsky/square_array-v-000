e3d9a51b56e099474960e26a6a311861550f6745
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-06-22 21:36:25 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,5 @@
-array = [2,3,4]
-
 def square_array(array)
-
-  #new_array = []
-  #array.each {|num| new_array << num ** 2}
-  #new_array
-
-  array.collect {|num| num ** 2}
-  array.display
+  new_array = []
+  array.each {|num| new_array << num ** 2}
+  new_array
 end
